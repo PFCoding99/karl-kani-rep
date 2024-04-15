ef0c54684a62ba1cf1860ca8e354aa4dabab69e3
PFCoding99 <PFCoding99@gmail.com>
2024-04-15 18:53:31 +0100

    fixed reuse nav

--- components/nav.js
+++ components/nav.js
@@ -4,47 +4,52 @@ class Nav extends HTMLElement {
   }
 
   connectedCallback() {
-    this.innerHTML = `<style>
-          nav {
-            height: 60px;
-            display: flex;
-            align-items: ;
-            justify-content: center;
-            background-color:  #0000;
-          
-          }
+    this.innerHTML = `<style>nav {
+      height: 70px;
+      display: flex;
+      align-items: left; 
+      justify-content: space-between; 
+      background-color:  #0000;
+    
+    }
   
-          ul {
-            padding: 0;
-          }
+    .NavUL {
+      padding: 0;
+      list-style-type: none;
+      display:flex;
+      flex-direction:row;
+    }
   
-          a {
-            font-weight: 700;
-            margin: 0 5px;
-            color: #000000;
-            text-decoration: none;
-            
-          }
+    a {
+      font-weight: 700;
+      margin: 0 5px;
+      color: #000000;
+      text-decoration: none;
+      padding:10px;
+      
+    }
   
-          a:hover {
-            padding-bottom: 5px;
-            box-shadow: inset 0 -2px 0 0 #fff;
-          }
-        </style>
-        <header>
-        <nav class="myNav">
-        <a href="index.html"></a>
-        <img src="../assets/img/karl-kani-logo-v1_b66bf179-b568-4392-9e4b-dee17e84d634.png" />
-        <ul> 
-          <li><a href="BIB.html">B.I.B COLLECTION</a></li>
-          <li><a href="LIFE.html">LIFE COLLECTION</a></li>
-          <li><a href="men.html">MEN</a></li>
-          <li><a href="women.html">WOMEN</a></li>
-          <li><a href="acc.html">ACCESSORIES</a></li>
-          <li><a href="sale.html">SALE</a></li>
-        </ul>
-      </nav>
-        </header>
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #fff;
+    }
+    
+    </style>
+    <header>
+    <nav class="myNav">
+    <a href="index.html"></a>
+    <img src="../assets/img/karl-kani-logo-v1_b66bf179-b568-4392-9e4b-dee17e84d634.png" />
+    <ul class="NavUL"> 
+      <li><a href="BIB.html">B.I.B COLLECTION</a></li>
+      <li><a href="LIFE.html">LIFE COLLECTION</a></li>
+      <li><a href="men.html">MEN</a></li>
+      <li><a href="women.html">WOMEN</a></li>
+      <li><a href="acc.html">ACCESSORIES</a></li>
+      <li><a href="sale.html">SALE</a></li>
+    </ul>
+  </nav>
+    </header>
+       
       `;
   }
 }
